853afc219cb1bbf6b77174ca9277b1f3eb75878f
Santiago Hernandez <santiago224z90@gmail.com>
2024-04-27 11:07:57 -0500

    RePlay

---
+++ app/src/main/java/com/example/replay/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.replay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnIngresar;
+    EditText txtUser, txtPassword;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPass);
+        btnIngresar = findViewById(R.id.btnIngresar);
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtUser.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+                if (email.isEmpty() && password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Ingresar los datos",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(email, password);
+                }
+            }
+        });
+    }
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            finish();
+                            startActivity(new Intent(MainActivity.this,newLayout.class));
+                            Toast.makeText(MainActivity.this,"BIENVENIDO",Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Si el inicio de sesión falla, mostrar un mensaje al usuario
+                            Toast.makeText(MainActivity.this, "Error al iniciar sesión: " +
+                                    task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"Error al iniciar sesión",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/replay/newLayout.java
@@ -0,0 +1,19 @@
+package com.example.replay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+
+public class newLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_layout);
+    }
+
+
+}
